f8dea5d0414d909327d87017080df09fd5011efe
Lvcas11 <138214108+Lvcas11@users.noreply.github.com>
2024-06-30 18:16:00 +0200

    adding dotenv to deployment vercel

--- tema-8/webpack.common.js
+++ tema-8/webpack.common.js
@@ -1,40 +1,46 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: './src/index.ts',
-  target: 'node',
-  externals: [nodeExternals()],
+  target: 'node', // Específicamente para aplicaciones Node.js
+  externals: [nodeExternals()], // Excluye dependencias de node_modules en el bundle
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: ['.ts', '.js'], // Permite importar archivos sin especificar la extensión
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader',
+        use: 'ts-loader', // Compila TypeScript a JavaScript
         exclude: /node_modules/
       },
       {
-        enforce: 'pre',
+        enforce: 'pre', // Ejecuta el cargador antes de otros cargadores
         test: /\.js$/,
-        loader: 'source-map-loader',
+        loader: 'source-map-loader', // Carga mapas de origen para ayudar en la depuración
         exclude: /node_modules/
       }
     ]
   },
   plugins: [
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin(), // Limpia el directorio de salida antes de cada compilación
     new CopyWebpackPlugin({
       patterns: [
-        { from: path.resolve(__dirname, 'src/config/.env'), to: '.env' }
+        { from: path.resolve(__dirname, 'src/config/.env'), to: '.env' } // Copia el archivo .env al directorio de salida
       ]
+    }),
+    new Dotenv({
+      path: path.resolve(__dirname, 'src/config/.env'), // Ruta a tu archivo .env
+      safe: true, // Opcional: Verifica que todas las variables definidas en .env.example estén presentes en .env
+      systemvars: true // Habilita el uso de variables de entorno del sistema si no se encuentran en el archivo .env
     })
   ]
 };
